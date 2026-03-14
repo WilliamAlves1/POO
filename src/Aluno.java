d31f0c376513cfe01baefc08203d7109d469b184
WilliamAlves1 <williamalvesg02@gmail.com>
2026-03-14 08:35:08 -0300

    .

---
+++ src/Aluno.java
@@ -0,0 +1,16 @@
+public class Aluno {
+
+    private int id;
+    private String nomeAluno;
+    private String numeroTelefone;
+
+    public Aluno (int id, String nomeAluno, String numeroTelefone){
+
+        this.id = id;
+        this.nomeAluno = nomeAluno;
+        this.numeroTelefone = numeroTelefone;
+
+
+
+    }
+}
